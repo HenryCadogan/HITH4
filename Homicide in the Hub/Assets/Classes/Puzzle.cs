a4c110ae8d91bce63aae31f63844bdc151d8aea9
HenryCadogan <hgc509@york.ac.uk>
2017-03-02 12:57:35 +0000

    Updated Gitignore to ignore .meta files Added in Json Object to the project Added the puzzle class components which is fully functional now (needs more riddles to be added in to fully test) Created the Puzzle scene (just to test classes, needs finishing)

--- Homicide in the Hub/Assets/Classes/Puzzle.cs
+++ Homicide in the Hub/Assets/Classes/Puzzle.cs
@@ -1,16 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using Assets.Classes;
 using UnityEngine;
+/// <summary>
+/// Class to be instantiated everytime a new puzzle is to be solved. The puzzle will load in the text from a JSON file each instance.
+/// </summary>
+public class Puzzle
+{
 
-public class Puzzle : MonoBehaviour {
+    private string riddleText, correctAnswer;
+    private List<string> wrongAnswers;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+    public Puzzle()
+    {
+        wrongAnswers = new List<string>();
+        LoadJSON();
+
+    }
+
+    private void LoadJSON()
+    {
+        JSONObject jo = new JSONObject(File.ReadAllText("Riddles.JSON"));
+        //pick random from keys
+        int index = Random.Range(0, jo.keys.Count);
+        //set the riddle object for simplicity
+        JSONObject riddle = jo.GetField(jo.keys[index]);
+        //set the riddle text
+        riddleText = riddle.GetField("RiddleText").str;
+        //set the correct answer text
+        correctAnswer = riddle.GetField("CorrectAnswer").str;
+        //set the list of incorrect answers
+        foreach (var line in riddle.GetField("WrongAnswers").list)
+        {
+            wrongAnswers.Add(line.str);
+        }
+
+        Debug.Log(riddleText);
+        Debug.Log(correctAnswer);
+        Debug.Log(wrongAnswers.ToString());
+
+    }
+
+    public string GetCorrectAnwer()
+    {
+        return correctAnswer;
+    }
+
+
+    public string GetRiddleText()
+    {
+        return riddleText;
+    }
+
+
 }
